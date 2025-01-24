f8b10370946a47a6436f6061aa08b4b393d0ff2e
PetrosB123 <pnblankenstein@willamette.edu>
2025-01-23 17:59:25 -0800

    1/23 self work

--- datastructures/bag.py
+++ datastructures/bag.py
@@ -9,28 +9,49 @@ class Bag(IBag[T]):
         if items is not None:
             for item in items:
                 self.add(item)
-                
 
     def add(self, item: T) -> None:
-        if item in self.__bag:
-            self.__bag[item] += 1
+        if item is None:
+            raise TypeError("cannot add 'None' to a Bag")
         else:
-            self.__bag[item] = 1
+            if item in self.__bag:
+                self.__bag[item] += 1
+            else:
+                self.__bag[item] = 1
 
     def remove(self, item: T) -> None:
-        raise NotImplementedError("remove method not implemented")
+        if item in self.__bag:
+            self.__bag[item] -= 1
+            if self.__bag[item] == 0:
+                del self.__bag[item]
+        else:
+            raise ValueError(f"'{item}' not found in Bag")
 
     def count(self, item: T) -> int:
-        raise NotImplementedError("count method not implemented")
+        if item in self.__bag:
+            return self.__bag[item]
+        else:
+            return 0
 
     def __len__(self) -> int:
-        raise NotImplementedError("__len__ method not implemented")
+        count = 0
+        for item in self.__bag:
+            count += self.__bag[item]
+        return count
 
     def distinct_items(self) -> Iterable[T]:
-        raise NotImplementedError("distinct_items method not implemented")
+        list = []
+        for item in self.__bag:
+            for i in range(self.count(item)):
+                list.append(item)
+        return list
 
     def __contains__(self, item) -> bool:
-        raise NotImplementedError("__contains__ method not implemented")
+        if item in self.__bag:
+            return True
+        else:
+            return False
 
     def clear(self) -> None:
-        raise NotImplementedError("clear method not implemented")
+        for item in self.__bag:
+            del self.__bag[item]
