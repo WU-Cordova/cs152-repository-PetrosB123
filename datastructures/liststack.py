84cccbe9e6ee9835b8ea0cc0239ee54fbc9ed6b2
PetrosB123 <pnblankenstein@willamette.edu>
2025-04-17 15:55:16 -0700

    Deque & ListStack

---
+++ datastructures/liststack.py
@@ -0,0 +1,136 @@
+import os
+from datastructures.istack import IStack
+from typing import Generic
+
+from datastructures.linkedlist import LinkedList
+
+class ListStack[T](Generic[T], IStack[T]):
+    """
+    ListStack (LinkedList-based Stack)
+
+    """
+
+    def __init__(self, data_type:object) -> None:
+        """
+        Initializes the ListStack.
+
+        Args:
+            data_type (type): The type of data the stack will hold.
+
+        """
+        self.list = LinkedList(data_type)
+
+    def push(self, item: T):
+        """
+        Pushes an item onto the stack.
+
+        Args:
+            item (T): The item to push onto the stack.
+        
+        Raises:
+            TypeError: If the item is not of the correct type.
+
+        """
+        self.list.append(item)
+
+    def pop(self) -> T:
+        """
+        Removes and returns the top item from the stack.
+
+        Returns:
+            T: The top item from the stack.
+        
+        Raises:
+            IndexError: If the stack is empty.
+        """
+        return self.list.pop()
+
+    def peek(self) -> T:
+        """
+        Returns the top item from the stack without removing it.
+
+        Returns:
+            T: The top item from the stack.
+        
+        Raises:
+            IndexError: If the stack is empty.
+        """
+        return self.list.back
+
+    @property
+    def empty(self) -> bool:
+        """
+        Checks if the stack is empty.
+
+        Returns:
+            bool: True if the stack is empty, False otherwise.
+        """
+        return self.list.empty
+
+    def clear(self):
+        """
+        Clears all items from the stack.
+        """
+        self.__init__(self.list.data_type)
+
+    def __contains__(self, item: T) -> bool:
+        """
+        Checks if an item exists in the stack.
+
+        Args:
+            item (T): The item to check for.
+
+        Returns:
+            bool: True if the item exists in the stack, False otherwise.
+
+        """
+        return self.list.__contains__(item)
+
+    def __eq__(self, other) -> bool:
+        """
+        Compares two stacks for equality.
+
+        Args:
+            other (ListStack): The stack to compare with.
+
+        Returns:
+            bool: True if the stacks are equal, False otherwise.
+
+        """
+        if isinstance(other, ListStack):
+            return self.list == other.list
+        else:
+            return False
+
+    def __len__(self) -> int:
+        """
+        Returns the number of items in the stack.
+
+        Returns:
+            int: The number of items in the stack.
+        """
+        return self.list.__len__()
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the stack.
+
+        Returns:
+            str: A string representation of the stack.
+        """
+        return self.list.__str__()
+
+    def __repr__(self) -> str:
+        """
+        Returns a detailed string representation of the stack.
+
+        Returns:
+            str: A detailed string representation of the stack.
+
+        """
+        return self.list.__repr__()
+    
+
+if __name__ == '__main__':
+    filename = os.path.basename(__file__)
+    print(f'OOPS!\nThis is the {filename} file.\nDid you mean to run your tests or program.py file?\nFor tests, run them from the Test Explorer on the left.')
